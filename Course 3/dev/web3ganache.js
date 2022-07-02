804ce9c93fd1f8f1910fdadb242a8311cebcabdb
adnanimeri <a.imeri@live.com>
2022-07-02 08:34:44 +0200

    new course

---
+++ Course 3/dev/web3ganache.js
@@ -0,0 +1,4 @@
+var Web3 = require('web3')
+var web3 = new Web3('http://127.0.0.1:7545')
+web3
+console.log(web3) 
---
+++ Course 3/dev/web3infura.js
@@ -0,0 +1,8 @@
+
+var Web3 = require('web3')
+var url = 'https://mainnet.infura.io/v3/d79e79983f574612b85eefebcdd35e6e'
+var web3 = new Web3(url)
+//print all web3 methods. ...
+console.log(web3)
+
+
